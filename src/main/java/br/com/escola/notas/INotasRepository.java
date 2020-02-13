91f6061d52e40f8f4c50ea73023e669ae9577bb4
thiago.prebibaj <thiagoprebibaj@hotmail.com>
2020-02-13 09:43:03 -0300

    Export de aluno escolhido e nome das matérias

--- src/main/java/br/com/escola/notas/INotasRepository.java
+++ src/main/java/br/com/escola/notas/INotasRepository.java
@@ -1,5 +1,6 @@
 package br.com.escola.notas;
 
+import br.com.escola.aluno.Alunos;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,10 +10,7 @@ import java.util.List;
 public interface INotasRepository extends JpaRepository <Notas, Long> {
 
   @Query(value = "SELECT * FROM seg_notas WHERE id_aluno = :idAluno", nativeQuery = true)
-  List<Notas> findByIdAluno(
-          @Param("idAluno") Long idAluno
-
-  );
+  List<Notas> findByAluno(Alunos idAluno);
 
 
 }
